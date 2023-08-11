a494c748305208a7577006ebe3b53e6f88ee5380
Lucas Sandin <lucasfernandessandin@gmail.com>
2023-08-11 09:58:15 -0300

    update

--- src/app/context/PostContext.tsx
+++ src/app/context/PostContext.tsx
@@ -26,9 +26,9 @@ export function PostsProvider({ children }: { children: React.ReactNode}) {
   const getPosts = async (queryParams: SubredditProps = defaultParams) => {
     const now = new Date();
     const response = await fetch(`${URL_BASE}?${toQueryParams(queryParams)}`, {
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36',
-      },
+      // headers: {
+      //   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36',
+      // },
       cache: 'no-store',
     });
 
